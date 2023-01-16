254f42b561427dfe14f6597f4b377bc1567232e6
klam2k20 <klam2k17@gmail.com>
2023-01-16 16:51:19 -0500

    Basic styling for header and searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { MdOutlineSearch } from 'react-icons/md';
 
+import '../styles/Searchbar.css';
+
 function Searchbar() {
   return (
     <div className="searchbar">
       <MdOutlineSearch />
-      <input type="text" />
+      <input type="text" placeholder="Search..." />
     </div>
   );
 }
